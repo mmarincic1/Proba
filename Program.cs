fb93c9b5454f21cf986eafeb5257f53b257a5cb3
Matej <mmarincic1@etf.unsa.ba>
2021-03-14 18:35:31 +0100

    Prvi zadacic

--- Program.cs
+++ Program.cs
@@ -6,7 +6,29 @@ namespace Proba
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Unesite 6 cijelih brojeva: ");
+            string unos = Console.ReadLine();
+            string[] brojeviString = unos.Split(",");
+            int[] cijeliBrojevi = new int[6];
+            int[] neparniBrojevi = new int[6];
+            bool pozitivni = true;
+            int j = 0;
+            for (int i = 0; i < cijeliBrojevi.Length; i++) {
+                cijeliBrojevi[i] = Int32.Parse(brojeviString[i]);
+                if(cijeliBrojevi[i] < 0) pozitivni = false;
+                if (cijeliBrojevi[i] % 2 != 0) neparniBrojevi[j++] = cijeliBrojevi[i];
+            }
+
+            if (!pozitivni) Console.WriteLine("Nisu svi brojevi pozitivni!");
+            else Console.WriteLine("Svi brojevi su pozitivni!");
+
+            // dobra caka sa ternarnim operatorom
+            /*
+             string ispis = pozitivni ? "Svi su pozitivni" : "Nisu svi pozitivni";
+             */
+            
+            /*Dobra caka sa string.Join*/
+            Console.WriteLine("Neparni brojevi su: " + string.Join(", ", neparniBrojevi[0..j]));
         }
     }
 }
